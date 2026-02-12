ec4b158042ab68b90a581b432dfd2d1c8953e58b
Yashwanth <yashgit16@gmail.com>
2026-02-12 23:05:13 +0530

    Remove all files

--- src/data_fetcher.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-def fetch_company_data(ticker):
-    print(f"[LookUP] Fetching financial data for: {ticker}")
-    stock = yf.Ticker(ticker)
-    info = stock.info
-    history = stock.history(period="1y")
-    return info, history
--- src/sector_utils.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-
-def get_sector_industry(ticker: str):
-    info = yf.Ticker(ticker).info
-    return {
-        "sector": info.get("sector"),
-        "industry": info.get("industry")
-    }
